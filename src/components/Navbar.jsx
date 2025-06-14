944652e3de023a91470aa6ef78689276b9ffe1ff
Farouk <bouazizfarouk3@gmail.com>
2025-06-14 18:15:43 +0100

    Created header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+
+  const toggleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <header className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      
+      <h1 className="text-3xl font-bold text-[#00df9a]">React.</h1>
+
+      <nav className={"fixed top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:static md:w-auto md:h-auto " + (nav ? "left-0" : "left-[-100%]")}>
+        <h1 className="block md:hidden text-3xl font-bold text-[#00df9a] m-4">React.</h1>
+        
+        <ul className="uppercase md:flex">
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </nav>
+
+      <button onClick={toggleNav} className="block md:hidden">
+        {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/> }
+      </button>
+    </header>
+  )
+}
+
+export default Navbar
+
